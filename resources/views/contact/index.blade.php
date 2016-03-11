6a4a7ffe53e338f0d8cd0e22a28f32d3a2511593
Darin Webb <dwebb@sourceallies.com>
2016-03-11 09:14:09 -0600

    table style

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -1,33 +1,31 @@
 @extends('master')
 @section('content')
     <h1>Contact Index</h1>
-    <div class="container">
-        <div class="col-md-6">
-            {{ var_dump($contacts) }}
-        </div>
-        <div class="col-md-6">
-            <table>
-                <thead>
+    <div class="col-md-6">
+        {{ var_dump($contacts) }}
+    </div>
+    <div class="col-md-6">
+        <table class="table table-striped table-bordered">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                </tr>
+            </thead>
+            <tbody>
+                @forelse($contacts as $contact)
                     <tr>
-                        <th>ID</th>
-                        <th>Name</th>
-                        <th>Email</th>
+                        <td>{{ $contact->id }}</td>
+                        <td>{{ $contact->name }}</td>
+                        <td>{{ $contact->email }}</td>
                     </tr>
-                </thead>
-                <tbody>
-                    @forelse($contacts as $contact)
-                        <tr>
-                            <td>{{ $contact->id }}</td>
-                            <td>{{ $contact->name }}</td>
-                            <td>{{ $contact->email }}</td>
-                        </tr>
-                    @empty
-                        <tr>
-                            <td col="4">Sorry, No Contacts Found</td>
-                        </tr>
-                    @endforelse
-                </tbody>
-            </table>
-        </div>
+                @empty
+                    <tr>
+                        <td col="4">Sorry, No Contacts Found</td>
+                    </tr>
+                @endforelse
+            </tbody>
+        </table>
     </div>
 @endsection
