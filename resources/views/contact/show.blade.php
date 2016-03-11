491f3dfd71055435c09b0f72145ce98a7dbab37f
Darin Webb <dwebb@sourceallies.com>
2016-03-11 09:16:27 -0600

    contact show style

--- resources/views/contact/show.blade.php
+++ resources/views/contact/show.blade.php
@@ -1,7 +1,12 @@
 @extends('master')
 @section('content')
     <h1>Contact Index</h1>
-    <p><b>id</b>: {{ $contact->id }}</p>
-    <p><b>email</b>: {{ $contact->email }}</p>
-    <p><b>name</b>: {{ $contact->name }}</p>
+    <div class="col-md-6">
+        {{ var_dump($contact) }}
+    </div>
+    <div class="col-md-6">
+        <p><b>id</b>: {{ $contact->id }}</p>
+        <p><b>email</b>: {{ $contact->email }}</p>
+        <p><b>name</b>: {{ $contact->name }}</p>
+    </div>
 @endsection
